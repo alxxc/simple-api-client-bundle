4a63f1c13500b04e19f2182e5a8aa096d05fd083
alx <alxphp@gmail.com>
2017-12-09 17:08:27 +0300

    integration tests

--- Service/ApiClient.php
+++ Service/ApiClient.php
@@ -12,6 +12,33 @@ use Symfony\Component\Serializer\Serializer;
 class ApiClient
 {
     /**
+     * @var string|null
+     */
+    private $baseUri = null;
+
+    /**
+     * @var int
+     */
+    private $timeout = 0;
+
+    /**
+     * @var bool
+     */
+    private $allow_redirects = false;
+
+    /**
+     * @var string|null
+     */
+    private $proxy = null;
+
+    public function __construct($timeout = 0)
+    {
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * Load locations info from remote uri
+     *
      * @param string $uri
      * @return Location[]
      * @throws BadResponseException
@@ -19,11 +46,17 @@ class ApiClient
      */
     public function loadLocations($uri)
     {
-        $client = new \GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $this->baseUri,
+            'timeout' => $this->timeout,
+            'allow_redirects' => $this->allow_redirects,
+            'proxy' => $this->proxy,
+        ]);
+
         $response = $client
             ->request('GET', $uri);
 
-        echo $response->getStatusCode(); // 200
+        // @todo process status code? $response->getStatusCode(); // 200
 
         $body = $response->getBody();
 
@@ -44,10 +77,13 @@ class ApiClient
 
         if (isset($jsonData->success)) {
             if ($jsonData->success) {
-                return $jsonData->data->locations;
-            } elseif (!empty($jsonData->code)) {
-                // @todo process message
-                throw new BadResponseException($jsonData->code);
+                if (!empty($jsonData->data) && !empty($jsonData->data->locations)) {
+                    return $jsonData->data->locations;
+                } else {
+                    throw new ResponseMalformedException('Bad data format');
+                }
+            } elseif (!empty($jsonData->message)) {
+                throw new BadResponseException($jsonData->message);
             } else {
                 throw new ResponseMalformedException('Error code is empty');
             }
@@ -69,5 +105,78 @@ class ApiClient
         ]);
 
         return $serializer->denormalize($data, Location::class.'[]');
+    }
+
+
+    /**
+     * @return null
+     */
+    public function getBaseUri()
+    {
+        return $this->baseUri;
+    }
+
+    /**
+     * @param null $baseUri
+     * @return self
+     */
+    public function setBaseUri($baseUri)
+    {
+        $this->baseUri = $baseUri;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    /**
+     * @param int $timeout
+     * @return self
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllowRedirects()
+    {
+        return $this->allow_redirects;
+    }
+
+    /**
+     * @param bool $allow_redirects
+     * @return self
+     */
+    public function setAllowRedirects($allow_redirects)
+    {
+        $this->allow_redirects = $allow_redirects;
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getProxy()
+    {
+        return $this->proxy;
+    }
+
+    /**
+     * @param null $proxy
+     * @return self
+     */
+    public function setProxy($proxy)
+    {
+        $this->proxy = $proxy;
+        return $this;
     }
 }
--- Tests/ApiClientIntegrationTest.php
+++ Tests/ApiClientIntegrationTest.php
@@ -6,19 +6,57 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ApiClientIntegrationTest extends KernelTestCase
 {
-    public function testIndex()
+    private $apiClient;
+    private $baseUri = 'http://localhost:8000/';
+
+    protected function setUp()
     {
         $kernel = static::bootKernel();
-        $apiClient = $kernel->getContainer()->get('simple_api_client.client');
-        $locations = $apiClient->loadLocations('http://127.0.0.1:8000/testapi/s1.json');
+        $this->apiClient = $kernel->getContainer()->get('simple_api_client.client')
+            ->setBaseUri($this->baseUri);
+    }
+
+    public function testOneEntity()
+    {
+        $locations = $this->apiClient->loadLocations('s1.json');
 
         $this->assertCount(1, $locations);
 
-        $location = reset($locations);
-        $this->assertEquals($location->getName(), 'Eiffel Tower');
+        $this->assertLocation(reset($locations), 'Eiffel Tower', 21.12, 19.56);
+    }
+
+    public function testSomeEntities()
+    {
+        $locations = $this->apiClient->loadLocations('s2.json');
+
+        $this->assertCount(2, $locations);
+
+        $this->assertLocation(reset($locations), 'Eiffel Tower', 21.12, 19.56);
+        $this->assertLocation(next($locations), 'The Statue of Liberty', 40.69, 74.03);
+    }
+
+    private function assertLocation($location, $nameExpected, $latExpected, $longExpected)
+    {
+        $this->assertEquals($location->getName(), $nameExpected);
 
         $coordinates = $location->getCoordinates();
-        $this->assertEquals($coordinates->getLong(), 19.56);
-        $this->assertEquals($coordinates->getLat(), 21.12);
+        $this->assertEquals($coordinates->getLong(), $longExpected);
+        $this->assertEquals($coordinates->getLat(), $latExpected);
+    }
+
+    /**
+     * @expectedException SimpleApiClientBundle\Exception\ResponseMalformedException
+     */
+    public function testGoodFormattedError()
+    {
+        $this->apiClient->loadLocations('f1.json');
+    }
+
+    /**
+     * @expectedException SimpleApiClientBundle\Exception\ResponseMalformedException
+     */
+    public function testBadFormattedError()
+    {
+        $this->apiClient->loadLocations('f2.json');
     }
 }
